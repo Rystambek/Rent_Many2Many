7cb815ad31235adcbb5a89082befc5d6cd740343
Rystambek <rustambekjumaqulov@gmail.com>
2023-12-05 21:32:39 +0500

    remove

--- group/urls.py
+++ group/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import Get_GroupsView,GruopId_All_Students
+from .views import Get_GroupsView,GruopId_All_Students,remove
 
 urlpatterns = [
     path('',Get_GroupsView.as_view()),
     path('<int:id>',Get_GroupsView.as_view()),
-    path('<int:id>/students',GruopId_All_Students.as_view())
+    path('<int:id>/students',GruopId_All_Students.as_view()),
+    path('<int:gr_id>/remove/<int:id>',remove.as_view()),
 ]
