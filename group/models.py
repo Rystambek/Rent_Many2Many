a5dd38bfc5315433d7889113c9dbdef0d1b5918c
Rystambek <rustambekjumaqulov@gmail.com>
2023-12-05 20:47:40 +0500

    group model

--- group/models.py
+++ group/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+from app.models import Customer
+
+class Group(models.Model):
+    name = models.CharField(max_length=20,unique=True)
+    student = models.ManyToManyField(Customer)
+
+    def __str__(self) -> str:
+        return self.name
