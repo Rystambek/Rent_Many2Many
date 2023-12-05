af893498f6e1807e90104475b4511ab57a760f64
Rystambek <rustambekjumaqulov@gmail.com>
2023-12-05 20:55:20 +0500

    gr id all student

--- group/urls.py
+++ group/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Get_GroupsView
+from .views import Get_GroupsView,GruopId_All_Students
 
 urlpatterns = [
-    path('gr/',Get_GroupsView.as_view()),
-    path('gr/<int:id>',Get_GroupsView.as_view())
+    path('',Get_GroupsView.as_view()),
+    path('<int:id>',Get_GroupsView.as_view()),
+    path('<int:id>/students',GruopId_All_Students.as_view())
 ]
